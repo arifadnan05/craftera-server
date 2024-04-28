396d316bf4b7d07db165e824cd84715f449b07e5
arifadnan05 <arifahamedadnan@gmail.com>
2024-04-28 10:02:19 +0600

    dynamic my added craft item api created

--- index.js
+++ index.js
@@ -66,6 +66,11 @@ async function run() {
       res.send(result)
     })
 
+    // dynamic craft item my item
+    app.get('/craft-item/:email', async (req, res) => {
+      const result = await craftItemCollection.find({ userEmail: req.params.email }).toArray();
+      res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
