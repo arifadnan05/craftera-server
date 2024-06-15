a8327ea6327f61151e32afa09746cb50659d65f9
arifadnan05 <arifahamedadnan@gmail.com>
2024-06-15 20:13:32 +0600

    environment variable setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,18 +8,16 @@ const port = process.env.PORT || 5000;
 
 
 //middleware
-
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://craftera-29a5a.web.app", "https://craftera-29a5a.firebaseapp.com"]
+  })
+)
 app.use(express.json());
 
 
 
-
-
-
-
-
-const uri = "mongodb+srv://craftera:ZFVSKEJjzFUZGYrG@cluster0.8lcgwxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8lcgwxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     const userCollection = client.db('userDB').collection('user')
     const craftItemCollection = client.db('userDB').collection('craft-item')
